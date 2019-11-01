cb6e3280e2db4e0e5151316bf7786c58cb1753e9
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-11-01 15:50:57 +0100

    added some views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,46 +3,46 @@
 @section('content')
 
 
-    <h1>List of People</h1>
+<h1>List of People</h1>
 
-    @foreach($person as $person)
-        <div> 
-            <h3>
-                {{$person->first_name}} {{$person->surname}}
-            </h3>
-            <div>
-                <p>
-                    {{$person->address}}
-                </p>
-                <p>
-                    {{$person->phone}}
-                </p>
-                <p>
-                    {{$person->email}}
-                </p>
-            </div>
-            <div>
-                @foreach($person->pets as $pet)
-                    <div>
-                        <h4>
-                            {{$pet->name}}
-                        </h4>
-                        <img src="{{$pet->photo->file_path}}" alt="">
-                        <p>
-                            {{$pet->breed}}
-                        </p>
-                        <p>
-                            {{$pet->weight}}
-                        </p>
-                        <p>
-                            {{$pet->age}}
-                        </p>
-                    </div>
-                  @endforeach  
-            </div>
+@foreach($person as $person)
+<div>
+    <h3>
+        {{$person->first_name}} {{$person->surname}} <a href="{{   action('PersonController@show', $person->first_name)     }}"></a>
+    </h3>
+    <div>
+        <p>
+            {{$person->address}}
+        </p>
+        <p>
+            {{$person->phone}}
+        </p>
+        <p>
+            {{$person->email}}
+        </p>
+    </div>
+    <div>
+        @foreach($person->pets as $pet)
+        <div>
+            <h4>
+                {{$pet->name}}
+            </h4>
+            <img src="{{$pet->photo->file_path}}" alt="">
+            <p>
+                {{$pet->breed}}
+            </p>
+            <p>
+                {{$pet->weight}}
+            </p>
+            <p>
+                {{$pet->age}}
+            </p>
         </div>
-    @endforeach
-        
+        @endforeach
+    </div>
+</div>
+@endforeach
+
 
 
 
--- resources/views/person/show.blade.php
+++ resources/views/person/show.blade.php
@@ -1,44 +1,8 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
 
-<h1>Create Person/Customer</h1>
-
-{{-- Errors --}}
-
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-
-<form action="{{  action('PersonController@store')  }}">
-    @csrf
-
-    <div class="form-group">
-        <label for="first_name">Name</label>
-        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="enter name...">
-
-        <label for="surname">Surname</label>
-        <input type="text" class="form-control" name="surname" id="surname" placeholder="enter surname...">
-
-        <label for="address">Address</label>
-        <input type="text" class="form-control" name="address" id="address" placeholder="enter address...">
-
-        <label for="phone">Phone</label>
-        <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone...">
-
-        <label for="email">Email</label>
-        <input type="text" class="form-control" name="email" id="email" placeholder="enter email...">
-
-        <br>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-
-</form>
+<h4 class="person-name">{{$person->first_name}} {{$person->surname}}</h4>
+<p>WILL BE UPDATED</p>
 
 @endsection
